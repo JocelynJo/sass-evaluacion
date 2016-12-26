d91ce22227e8fdf12a18b7c129dcf485d0be2c3b
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2016-12-26 01:09:59 -0800

    hasta seccion del video falta corregir navbar y busqueda ademas de el resto de las secciones

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('select').material.select();
+	$('.datapicker').pickadate();
+	$('.dropdown-button').dropdown();
+	$('.button-collapse').sideNav();
+	$('.slider').slider;
+});
